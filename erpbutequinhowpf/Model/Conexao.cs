c1c497796a8758b17515e37ad0775cf4cbdea692
Wallison da Silva Gregorio <wallisongregorio2020@gmail.com>
2020-10-21 21:51:29 -0300

    Conexao com banco de dados

---
+++ erpbutequinhowpf/Model/Conexao.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace erpbutequinhowpf.Model
+{
+    class Conexao
+    {
+
+        public int idcliente { get; set; }
+        public string nome { get; set; }
+        public string  cpf{ get; set; }
+        public string cnpj { get; set; }
+        public string telefone { get; set; }
+        public string logradouro { get; set; }
+        public string numero { get; set; }
+        public string complemento { get; set; }
+        public string bairro { get; set; }
+        public string cep { get; set; }
+        public string cidade { get; set; }
+        public string estado { get; set; }
+
+        public MySqlConnection connection { get; set; }
+        public MySqlCommand comando { get; set; }
+        public MySqlDataAdapter da { get; set; }
+        public MySqlDataReader  dr { get; set; }
+
+        public Conexao()
+        {
+        }
+
+        public Conexao(string nome, string cpf, string cnpj, string telefone, string logradouro, string numero, string complemento, string bairro, string cep, string cidade, string estado)
+        {
+            this.nome = nome;
+            this.cpf = cpf;
+            this.cnpj = cnpj;
+            this.telefone = telefone;
+            this.logradouro = logradouro;
+            this.numero = numero;
+            this.complemento = complemento;
+            this.bairro = bairro;
+            this.cep = cep;
+            this.cidade = cidade;
+            this.estado = estado;
+        }
+
+
+        protected MySqlConnection Conexao_Banco()
+        {
+            connection = new MySqlConnection("Server=localhost;Database=cadastro_cliente;Uid=root;Pwd=kekinho13;");
+            return connection;
+        }
+    }
+}
---
+++ erpbutequinhowpf/Model/To_Locate.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Windows;
+using System.Data;
+
+namespace erpbutequinhowpf.Model
+{
+    class To_Locate:Conexao
+    {
+        public string sql_select { get; set; }
+        public string sql_mostrar { get; set; }
+
+        protected void Localizar()
+        {
+            try
+            {
+                Conexao cn = new Conexao();
+
+                
+                sql_select = "SELECT * FROM cliente WHERE nome = @nome ";
+
+                cn.comando = new MySqlCommand(sql_select, Conexao_Banco());
+
+                cn.comando.Parameters.AddWithValue("@nome", cn.nome);
+
+                connection.Open();
+                dr = comando.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+                connection = null;
+                comando = null;
+            }
+        }
+
+        protected void Mostrar_Tabela()
+        {
+            try
+            {
+                Conexao cn = new Conexao();
+
+
+                sql_mostrar = "SELECT * FROM cliente ";
+
+                da = new MySqlDataAdapter (sql_mostrar, Conexao_Banco());
+
+                DataTable dt = new DataTable();
+
+                da.Fill(dt);
+
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+                connection = null;
+                comando = null;
+            }
+        }
+    }
+}
