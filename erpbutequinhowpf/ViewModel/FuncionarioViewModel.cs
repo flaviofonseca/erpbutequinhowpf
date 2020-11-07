5edae6cdf033ada1ed2f240f2709c6df589a3a2c
Flavio Fonseca Martins <flaviofonseca@prodata.inf.br>
2020-11-06 22:49:45 -0300

    ...

--- erpbutequinhowpf/ViewModel/FuncionarioViewModel.cs
+++ erpbutequinhowpf/ViewModel/FuncionarioViewModel.cs
@@ -1,15 +1,28 @@
-﻿using System;
+﻿using erpbutequinhowpf.Model;
+using System;
 
 namespace erpbutequinhowpf.ViewModel
 {
     class FuncionarioViewModel
     {
+        private FuncionarioModel funcionarioModel;
+
+        public FuncionarioViewModel()
+        {
+            this.funcionarioModel = new FuncionarioModel();
+        }
+
         internal void SalvarFuncionario(Funcionario funcionario)
         {
-            Console.WriteLine(funcionario);
+            this.funcionarioModel.Inserir(funcionario);
         }
 
         internal void RemoverFuncionario(Funcionario funcionario)
+        {
+            this.funcionarioModel.Delete(funcionario.Id);
+        }
+
+        internal void ConsultarPorId(int codigo)
         {
             throw new NotImplementedException();
         }
