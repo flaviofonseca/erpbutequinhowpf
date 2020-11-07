98897c47f07bb7fe984822b5301342acec7ccb36
Wallison da Silva Gregorio <wallisongregorio2020@gmail.com>
2020-11-06 22:19:22 -0300

    Atualização BD

--- erpbutequinhowpf/Model/Conexao.cs
+++ erpbutequinhowpf/Model/Conexao.cs
@@ -10,43 +10,10 @@ namespace erpbutequinhowpf.Model
     class Conexao
     {
 
-        public int idcliente { get; set; }
-        public string nome { get; set; }
-        public string  cpf{ get; set; }
-        public string cnpj { get; set; }
-        public string telefone { get; set; }
-        public string logradouro { get; set; }
-        public string numero { get; set; }
-        public string complemento { get; set; }
-        public string bairro { get; set; }
-        public string cep { get; set; }
-        public string cidade { get; set; }
-        public string estado { get; set; }
-
         public MySqlConnection connection { get; set; }
         public MySqlCommand comando { get; set; }
         public MySqlDataAdapter da { get; set; }
         public MySqlDataReader  dr { get; set; }
-
-        public Conexao()
-        {
-        }
-
-        public Conexao(string nome, string cpf, string cnpj, string telefone, string logradouro, string numero, string complemento, string bairro, string cep, string cidade, string estado)
-        {
-            this.nome = nome;
-            this.cpf = cpf;
-            this.cnpj = cnpj;
-            this.telefone = telefone;
-            this.logradouro = logradouro;
-            this.numero = numero;
-            this.complemento = complemento;
-            this.bairro = bairro;
-            this.cep = cep;
-            this.cidade = cidade;
-            this.estado = estado;
-        }
-
 
         protected MySqlConnection Conexao_Banco()
         {
