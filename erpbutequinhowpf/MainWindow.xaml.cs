e3af69499509efe55f824ad87fbcd126c5b5da6b
Flavio Fonseca Martins <flaviofonseca@prodata.inf.br>
2020-11-06 21:18:01 -0300

    mmm

--- erpbutequinhowpf/MainWindow.xaml.cs
+++ erpbutequinhowpf/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using erpbutequinhowpf.views.menu;
 
 namespace erpbutequinhowpf
@@ -46,12 +34,16 @@ namespace erpbutequinhowpf
             switch (((ListViewItem)((ListView)sender).SelectedItem).Name)
             {
                 case "ItemHome":
+                   // usc = new UserHomeControl();
+                    //GridMain.Children.Add(usc);
+                    break;
+                case "ItemCreate":
                     usc = new UserHomeControl();
                     GridMain.Children.Add(usc);
                     break;
-                case "ItemCreate":
-                   // usc = new UserControlCreate();
-                   // GridMain.Children.Add(usc);
+                case "ItemPesquisa":
+                   // usc = new UserPesquisaControl();
+                    GridMain.Children.Add(usc);
                     break;
                 default:
                     break;
